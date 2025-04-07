ab02048dad75980ee4038705afe04654dcc2201c
Alice Poltronieri <alice.snieri@hotmail.com>
2025-04-07 08:50:27 -0300

    agenda 07

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -1,24 +1,82 @@
-﻿namespace MauiAppTempoAgora
+﻿using MauiAppTempoAgora.Models;
+using MauiAppTempoAgora.Services;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MauiAppTempoAgora
 {
     public partial class MainPage : ContentPage
     {
         int count = 0;
 
+        private readonly HttpClient _httpClient = new HttpClient();
+
         public MainPage()
         {
             InitializeComponent();
+            CheckInternetConnection();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void CheckInternetConnection()
         {
-            count++;
+            if (!await IsInternetAvailable())
+            {
+                await DisplayAlert("Sem Conexão", "Você está sem conexão com a internet.", "OK");
+            }
+        }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+        private async Task<bool> IsInternetAvailable()
+        {
+            try
+            {
+                var response = await _httpClient.GetAsync("http://api.openweathermap.org/");
+                return response.IsSuccessStatusCode;
+            }
+            catch
+            {
+                return false;
+            }
+        }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(txt_cidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
+
+                    if(t != null)
+                    {
+                        string dados_previsao = "";
+
+                        dados_previsao = $"Latitude: {t.lat} \n" +
+                                         $"Longitude: {t.lon} \n" +
+                                         $"Nascer do Sol: {t.sunrise} \n" +
+                                         $"Por do Sol: {t.sunset} \n" +
+                                         $"Temp Máx: {t.temp_max} \n" +
+                                         $"Temp Min: {t.temp_min} \n" +
+                                         $"Descrição: {t.description} \n" +
+                                         $"Velocidade do vento: {t.speed}\n" +
+                                         $"Visibilidade: {t.visibility} \n";
+
+                        lbl_res.Text = dados_previsao;
+                    }
+                    else
+                    {
+                        lbl_res.Text = "Cidade não encontrada";
+                    }
+                }
+                else
+                {
+                    lbl_res.Text = "Preencha a cidade";
+                }
+                    
+
+            } catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
         }
     }
 
